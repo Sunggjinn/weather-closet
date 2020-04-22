ab066928415c0155c6c4860d8e9945ebf7110f36
Lime0 <61825404+Lime0@users.noreply.github.com>
2020-04-22 23:27:30 +0900

    Update weatherScrapping.py

--- weatherScrapping.py
+++ weatherScrapping.py
@@ -3,20 +3,28 @@ import urllib
 import bs4
 import html5lib
 
-location = '서울'
-enc_location = urllib.parse.quote(location + '날씨')
+input_area = '서울'
+enc_area = urllib.parse.quote(input_area + '날씨')
 
-url = 'https://search.naver.com/search.naver?ie=utf8&query='+ enc_location
+url = 'https://search.naver.com/search.naver?ie=utf8&query='+ enc_area
 
 req = Request(url)
 page = urlopen(req)
 html = page.read()
 soup = bs4.BeautifulSoup(html,'html5lib')
 
-today_temperature = soup.find('p', class_='info_temperature').find('span', class_='todaytemp').text
-today_weather_desc = soup.find('ul', class_='info_list').find('li').find('p', class_='cast_txt').text
+temp = soup.find('p', class_='info_temperature').find('span', class_='todaytemp').text
+weather_desc = soup.find('ul', class_='info_list').find('li').find('p', class_='cast_txt').text
 min_temp = soup.find('span', class_='min').find('span', class_='num').text
 max_temp = soup.find('span', class_='max').find('span', class_='num').text
-windchill = soup.find('span', class_='sensible').find('em').find('span', class_='num').text
 
-print(today_temperature, today_weather_desc, min_temp, max_temp, windchill)
+if '맑음' in weather_desc:
+  weather = 'sunny'
+elif '흐림' in weater_desc:
+  weather = 'cloudy'
+elif '비' in weather_desc:
+  weather = 'rainy'
+else:
+  weather = 'cloudful'
+
+print(temp, weather, min_temp, max_temp)
