55bf008087316b6fbcd8381bc762487988606790
Lime0 <61825404+Lime0@users.noreply.github.com>
2020-04-26 19:10:17 +0900

    20200426 layout/scrapper/app

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ app = Flask("Weather Scrapper")
 def home():
     area = request.args.get('area')
     if area:
-        weather = weather_search(area)
-        print(weather)
-    return render_template("index.html", searchingBy=area)
+        weather_element = weather_search(area)
+        print(weather_element)
+    return render_template("index.html", searchingBy=area, today_temp=weather_element[0], high_temp=weather_element[3], low_temp=weather_element[2])
 
 
 app.run ('0.0.0.0', port=5000)
---
+++ scrapper.py
@@ -0,0 +1,34 @@
+from urllib.parse import urlparse
+from urllib.request import urlopen, Request
+import urllib
+import bs4
+import requests
+import html5lib
+
+
+def weather_search(area):
+    input_area = area
+
+    enc_area = urllib.parse.quote(input_area+'날씨')
+    url = 'https://search.naver.com/search.naver?ie=utf8&query='+enc_area
+    req = Request(url)
+    page = urlopen(req)
+    html = page.read()
+    soup = bs4.BeautifulSoup(html, 'html5lib')
+
+    temp = soup.find('p', class_='info_temperature').find('span', class_='todaytemp').text
+    weather_desc = soup.find('ul', class_='info_list').find('li').find('p', class_='cast_txt').text
+    min_temp = soup.find('span', class_='min').find('span', class_='num').text
+    max_temp = soup.find('span', class_='max').find('span', class_='num').text
+
+    if '맑음' in weather_desc:
+        weather = 'sunny'
+    elif '흐림' in weather_desc:
+        weather = 'cloudy'
+    elif '비' in weather_desc:
+        weather = 'rainy'
+    else:
+        weather = 'cloudful'
+
+    weathers = [temp, weather, min_temp, max_temp]
+    return weathers
